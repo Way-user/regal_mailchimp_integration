9e7acd5b1944b16bbfb9dfb91a538b6aad346264
Way-user <sruthi.polina@way.com>
2025-03-11 14:56:02 -0500

    Update sync_service.py

--- sync_service.py
+++ sync_service.py
@@ -13,7 +13,7 @@ REGAL_IO_API_KEY = os.environ["REGAL_IO_API_KEY"]
 MAILCHIMP_API_KEY = os.environ["MAILCHIMP_API_KEY"]
 MAILCHIMP_LIST_ID = "2960f1c6f4"
 MAILCHIMP_DC = os.environ["MAILCHIMP_DC"]
-MAILCHIMP_CAMPAIGN_ID = "77dc497b28"
+MAILCHIMP_CAMPAIGN_ID = "3c0aa99a7a"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
